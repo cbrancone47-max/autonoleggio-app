9852a7b9be0f08393d6b87365c9702a0e38a13b5
cbrancone47-max <cbrancone47@gmail.com>
2026-08-19 20:15:02 +0200

    Update web_autonoleggio.py

--- web_autonoleggio.py
+++ web_autonoleggio.py
@@ -19,7 +19,7 @@ else:
 
 APPS_SCRIPT_URL = st.secrets.get(
     "APPS_SCRIPT_URL",
-    "https://script.google.com/macros/s/AKfycbw3ekPzkSNSqlwqHcLazt6hucWfrarrFLDY5yuI7V2iLm5dB6r2G23zG2ewSm3TQ-8ebA/exec",
+    "https://script.google.com/macros/s/AKfycbx1NskWEPApR1TXX9KOut1OJBKYqGYDnlAsRXpfLemc3bLXsSvmRUKkhjpbBz7wJ4NvuA/exec",
 )
 
 # Nomi colonne esatti dal Foglio Google
